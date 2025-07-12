8c7588c6606edc8a09ab96acec38f528c498e021
Lakshmi.Sanikommu <59156926+18121A05L2@users.noreply.github.com>
2025-07-12 16:14:29 +0530

    Empty repo

--- contracts/src/IVerifier.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.2;
-
-interface IVerifier {
-    function verify(bytes32 proof, bytes32[] calldata publicInputs) external returns (bool);
-}
